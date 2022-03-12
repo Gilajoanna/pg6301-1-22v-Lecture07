449600618760e3e0e64755e87129302cc13a9a15
Gilajoanna <gilajoannajohansson@gmail.com>
2022-03-12 14:39:18 +0100

    Add more filtering and sorting in list of movies

--- server/moviesApi.js
+++ server/moviesApi.js
@@ -5,7 +5,17 @@ export function MoviesApi(mongoDatabase) {
 
     router.get("/", async (req, res) => {
         const movies = await mongoDatabase.collection("movies")
-            .find()
+            .find({
+                countries: {
+                    $in: ["Ukraine"],
+                },
+                year: {
+                    $gte: 2000,
+                }
+            })
+            .sort({
+                metacritic: -1,
+            })
             .map(({title, year, plot, poster}) =>
                 ({title, year, plot, poster}))
             .limit(100)
