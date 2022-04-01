0770e03a9a44cd96db84e92e05a4c5d2ac789e65
Gilajoanna <gilajoannajohansson@gmail.com>
2022-04-01 19:55:52 +0200

    shows error message test green

--- client/__tests__/listMovies.test.jsx
+++ client/__tests__/listMovies.test.jsx
@@ -23,4 +23,23 @@ describe("ListMovies component", () => {
 
     expect(domElement.innerHTML).toMatchSnapshot();
   });
+
+  it("shows error message", async () => {
+    const domElement = document.createElement("div");
+    await act(async () => {
+      ReactDOM.render(
+        <ListMovies
+          listMovies={() => {
+            throw new Error("Something went wrong");
+          }}
+        />,
+        domElement
+      );
+    });
+
+    expect(domElement.querySelector("#error-text").innerHTML).toEqual(
+      "Error: Something went wrong"
+    );
+    expect(domElement.innerHTML).toMatchSnapshot();
+  });
 });
