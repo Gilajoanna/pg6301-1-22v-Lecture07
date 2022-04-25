8ea5b7df6a1532c6b020107a59f1dae1e4cd5b50
Gilajoanna <gilajoannajohansson@gmail.com>
2022-04-25 17:02:37 +0200

    Test for inserting new movies

--- server/__tests__/moviesApi.test.js
+++ server/__tests__/moviesApi.test.js
@@ -3,23 +3,46 @@ import express from "express";
 import { MongoClient } from "mongodb";
 import { MoviesApi } from "../moviesApi";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app = express();
+app.use(bodyParser.json());
 
+const mongoClient = new MongoClient(process.env.MONGODB_URL);
 beforeAll(async () => {
-  const mongoClient = new MongoClient(process.env.MONGODB_URL);
   await mongoClient.connect();
-  app.use("/api/movies", MoviesApi(mongoClient.db("sample_mflix")));
+  const database = mongoClient.db("test_database");
+  await database.collection("movies").deleteMany({});
+  app.use("/api/movies", MoviesApi(database));
+});
+afterAll(() => {
+  mongoClient.close();
 });
 
 describe("movies api", function () {
+  it("adds a new movie", async () => {
+    await request(app)
+      .post("/api/movies")
+      .send({
+        title: "My test movie",
+        country: "Sweden",
+        year: 1992,
+      })
+      .expect(200);
+    expect(
+      (await request(app).get("/api/movies").expect(200)).body.map(
+        ({ title }) => title
+      )
+    ).toContain("My test movie");
+  });
+
   it("Lists existing movies", async () => {
     expect(
       (await request(app).get("/api/movies").expect(200)).body.map(
         ({ title }) => title
       )
-    ).toContain("Force Majeure");
+    ).toContain("My test movie");
   });
 });
--- server/moviesApi.js
+++ server/moviesApi.js
@@ -1,40 +1,37 @@
-import {Router} from "express";
+import { Router } from "express";
 
 export function MoviesApi(mongoDatabase) {
-    const router = new Router();
+  const router = new Router();
 
-    router.get("/", async (req, res) => {
-        const movies = await mongoDatabase.collection("movies")
-            .find({
-                countries: {
-                    $in: ["Sweden"],
-                },
-                year: {
-                    $gte: 1991,
-                }
-            })
-            .sort({
-                metacritic: -1
-            }
-            )
-            .map(({title, year, plot, poster}) =>
-                ({title, year, plot, poster}))
-            .limit(100)
-            .toArray();
-        res.json(movies);
+  router.get("/", async (req, res) => {
+    const movies = await mongoDatabase
+      .collection("movies")
+      .find({
+        countries: {
+          $in: ["Sweden"],
+        },
+        year: {
+          $gte: 1991,
+        },
+      })
+      .sort({
+        metacritic: -1,
+      })
+      .map(({ title, year, plot, poster }) => ({ title, year, plot, poster }))
+      .limit(100)
+      .toArray();
+    res.json(movies);
+  });
+
+  router.post("/", (req, res) => {
+    const { title, country, year } = req.body;
+    const result = mongoDatabase.collection("movies").insertOne({
+      title,
+      countries: [country],
+      year,
     });
+    res.sendStatus(200);
+  });
 
-    router.post("/new", async (req, res) => {
-        const { title, year, countries, plot } = req.body;
-        const result = await mongoDatabase.collection("movies").insertOne({
-            title,
-            year,
-            countries,
-            plot
-        });
-        console.log({ result })
-        res.sendStatus(200);
-    });
-
-    return router;
+  return router;
 }
