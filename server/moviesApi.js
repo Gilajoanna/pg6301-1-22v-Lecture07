2c80954629f24f3a24efa664f8259d9f450ad62c
Gilajoanna <gilajoannajohansson@gmail.com>
2022-04-26 16:31:33 +0200

    fix up attempt 3. Moved out code from index to application.jsx.

--- server/moviesApi.js
+++ server/moviesApi.js
@@ -1,40 +1,36 @@
-import {Router} from "express";
+import { Router } from "express";
 
 export function MoviesApi(mongoDatabase) {
-    const router = new Router();
+  const router = new Router();
 
-    router.get("/", async (req, res) => {
-        const movies = await mongoDatabase.collection("movies")
-            .find({
-                countries: {
-                    $in: ["Sweden"],
-                },
-                year: {
-                    $gte: 1991,
-                }
-            })
-            .sort({
-                metacritic: -1
-            }
-            )
-            .map(({title, year, plot, poster}) =>
-                ({title, year, plot, poster}))
-            .limit(100)
-            .toArray();
-        res.json(movies);
+  router.get("/", async (req, res) => {
+    const movies = await mongoDatabase
+      .collection("movies")
+      .find({
+        countries: {
+          $in: ["Sweden"],
+        },
+        year: {
+          $gte: 1991,
+        },
+      })
+      .map(({ title, year, plot, poster }) => ({ title, year, plot, poster }))
+      .limit(100)
+      .toArray();
+    res.json(movies);
+  });
+
+  router.post("/new", async (req, res) => {
+    const { title, year, countries, plot } = req.body;
+    const result = await mongoDatabase.collection("movies").insertOne({
+      title,
+      year,
+      countries,
+      plot,
     });
+    console.log({ result });
+    res.sendStatus(200);
+  });
 
-    router.post("/new", async (req, res) => {
-        const { title, year, countries, plot } = req.body;
-        const result = await mongoDatabase.collection("movies").insertOne({
-            title,
-            year,
-            countries,
-            plot
-        });
-        console.log({ result })
-        res.sendStatus(200);
-    });
-
-    return router;
+  return router;
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import * as path from "path";
-import {MoviesApi} from "./moviesApi.js";
+import { MoviesApi } from "./moviesApi.js";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
@@ -12,23 +12,27 @@ app.use(bodyParser.json());
 
 const mongoClient = new MongoClient(process.env.MONGODB_URL);
 mongoClient.connect().then(async () => {
-    console.log("Connected to mongodb")
-    //const databases = await mongoClient.db().admin().listDatabases();
-    //console.log(databases);
+  console.log("Connected to mongodb");
+  //const databases = await mongoClient.db().admin().listDatabases();
+  //console.log(databases);
 
-    app.use("/api/movies", MoviesApi(mongoClient.db(process.env.MONGODB_DATABASE || "pg6301-lecture07")));
-
+  app.use(
+    "/api/movies",
+    MoviesApi(
+      mongoClient.db(process.env.MONGODB_DATABASE || "pg6301-lecture07")
+    )
+  );
 });
 
 app.use(express.static("../client/dist/"));
 app.use((req, res, next) => {
-    if(req.method === "GET" && !req.path.startsWith("/api")) {
-        return res.sendFile(path.resolve("../client/dist/index.html"));
-    } else {
-        next();
-    }
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    return res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server started on https://localhost:${server.address().port}`);
+  console.log(`Server started on https://localhost:${server.address().port}`);
 });
